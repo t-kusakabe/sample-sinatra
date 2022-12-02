48a64d0b2797f185843873036c488aa323338e59
t-kusakabe <ttmac0704@gmail.com>
2022-12-02 15:32:55 +0900

    sinatra/reloader入れる

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require 'sinatra/base'
+require 'sinatra/reloader'
 
 class SampleSinatra < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
   get '/' do
     'hello world!'
   end
