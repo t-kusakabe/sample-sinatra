6eae19a828fbf25c0905573fd15629a027637c5d
t-kusakabe <ttmac0704@gmail.com>
2022-12-03 21:40:21 +0900

    db接続系

--- model/application_record.rb
+++ model/application_record.rb
@@ -4,4 +4,7 @@ require 'active_record'
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  ActiveRecord::Base.configurations = YAML.load(ERB.new(File.read('./config/database.yml')).result)
+  ActiveRecord::Base.establish_connection(:development)
 end
