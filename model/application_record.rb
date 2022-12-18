e05852501e233bcbb3597c0733b323013620e7a6
t-kusakabe <ttmac0704@gmail.com>
2022-12-18 21:55:37 +0900

    YAMLの読み込み方を変える

--- model/application_record.rb
+++ model/application_record.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
+require 'psych'
 require 'active_record'
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  ActiveRecord::Base.configurations = YAML.load(ERB.new(File.read('./config/database.yml')).result)
+  ActiveRecord::Base.configurations = Psych.load(ERB.new(File.read('./config/database.yml')).result)
   ActiveRecord::Base.establish_connection(ENV['RACK_ENV'].to_sym)
 end
