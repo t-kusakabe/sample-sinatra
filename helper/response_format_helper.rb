dd93c3bf2c45b07c584055c831b2b2f5cb2a653a
t-kusakabe <ttmac0704@gmail.com>
2022-12-19 14:28:57 +0900

    深いnestのarrayにも対応する

--- helper/response_format_helper.rb
+++ helper/response_format_helper.rb
@@ -6,19 +6,21 @@ module ResponseFormatHelper
       def camelize(data)
         data.deep_transform_keys! { |k| k.camelize(:lower) }
       end
+
+      def array_in_inside?(array)
+        array.instance_of?(Array) ? array_in_inside?(array.first) : array
+      end
     end
   }
 
   def deep_camelize!
     body = response.body.first
 
-    if JSON.parse(body).instance_of?(Array)
-      return JSON.parse(body).map do |data|
-        camelize(data)
-      end
-    end
-
     return camelize(JSON.parse(body)) if JSON.parse(body).instance_of?(Hash)
+    return body unless JSON.parse(body).instance_of?(Array)
+
+    extracted_data = array_in_inside?(JSON.parse(body))
+    return camelize(extracted_data) if extracted_data.instance_of?(Hash)
 
     body
   end
